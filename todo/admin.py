e0610c7e3ba768ddaf889696d67f3e08897e0b03
zubaidullo <1704.01051@manas.edu.kg>
2021-03-03 17:43:29 +0600

    api work version with admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'discription', 'date', 'done')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'discription')
+    list_editable = (['done'])
+    list_filter = (['done'])
+
+admin.site.register(Todo, TodoAdmin)
